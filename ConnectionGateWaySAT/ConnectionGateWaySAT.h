744ca87ca5e88814621b6a758f3e43f53c10da01
Papon <papon.s@lawyerplusgroup.com>
2023-01-26 10:31:07 +0700

    Initial Commit

---
+++ ConnectionGateWaySAT/ConnectionGateWaySAT.h
@@ -0,0 +1,18 @@
+//
+//  ConnectionGateWaySAT.h
+//  ConnectionGateWaySAT
+//
+//  Created by Papon Supamongkonchai on 26/1/2566 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConnectionGateWaySAT.
+FOUNDATION_EXPORT double ConnectionGateWaySATVersionNumber;
+
+//! Project version string for ConnectionGateWaySAT.
+FOUNDATION_EXPORT const unsigned char ConnectionGateWaySATVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConnectionGateWaySAT/PublicHeader.h>
+
+
