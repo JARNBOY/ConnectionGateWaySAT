4203c9539475faa0a0dfbd0953023296e00eb607
Papon <papon.s@lawyerplusgroup.com>
2023-01-28 19:51:26 +0700

    [Demo SuperApp][Sample Framework Connect Standard] Create ConnectionGateWaySAT framework for define standard interface for all mini apps (Reviewed by Papon S.)

---
+++ ConnectionGateWaySAT/ConnectionGateWaySAT.swift
@@ -0,0 +1,14 @@
+//
+//  ConnectionGateWaySAT.swift
+//  ConnectionGateWaySAT
+//
+//  Created by Papon Supamongkonchai on 26/1/2566 BE.
+//
+
+import UIKit
+
+// Define standard interface for all mini apps
+public protocol ConnectionGateWaySAT {
+    init()
+    func createInputViewController() -> UIViewController?
+}
